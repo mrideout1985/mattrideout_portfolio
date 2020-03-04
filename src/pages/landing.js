7c0175228de67f1f65fe71fc0a36164f58d7bdac
Matthew Rideout <mrideout1985@gmail.com>
2020-03-04 17:06:04 +0000

    u

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -2,13 +2,9 @@ import React from "react";
 import Header from "../components/header/Header";
 import About from "../components/about/About";
 
-const Landing=()=> {
-    return (
-        <div>
-            <Header />
-            <About />
-        </div>
-    );
-}
+const Landing = () => (<div>
+    <Header />
+    <About />
+</div>)
 
 export default Landing;
