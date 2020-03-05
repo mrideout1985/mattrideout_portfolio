8caa2cefa1b205836cd9f6ab2b9d5b98f40f5b5b
Matthew <mrideout1985@gmail.com>
2020-03-05 15:45:24 +0000

    updated

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Projects from '../../pages/Projects'
 import Blog from '../../pages/Blog'
 import Writing from '../../pages/Writing';
-import Layout from '../layout/Layout';
+import Layout from '../layoutfile/Layout';
 import Landing from "../../pages/Landing";
 
 
