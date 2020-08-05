c91aa12624961eb7572937a03767bb1bf1ec2880
mrideout1985 <mrideout1985@gmail.com>
2020-08-05 15:46:44 +0100

    updated

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./About.module.scss";
 import instagram from "../../images/instagram.svg";
 import linkedin from "../../images/linkedin.svg";
